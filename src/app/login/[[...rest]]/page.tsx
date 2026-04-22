8ef2fabc6185217f203d46267b3c43eb393cc75a
Naperality <napolytan@gmail.com>
2026-04-22 10:43:52 +0800

    Initial

---
+++ src/app/login/[[...rest]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn, UserButton } from "@clerk/nextjs";
+
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <SignIn />
+    </div>
+  );
+}
